4e176e7a44296dba5d341f1fd1e9b59e354f425f
DanGoing <2506167296@qq.com>
2015-06-26 09:58:56 +0800

    init

---
+++ app/src/main/java/com/clover/ui/MainActivity.java
@@ -0,0 +1,183 @@
+package com.clover.ui;
+
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.clover.R;
+import com.clover.ui.frgms.GamePage;
+import com.clover.ui.frgms.MainPage;
+import com.clover.ui.frgms.UserPage;
+
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity {
+    private ViewPager mPager;//ҳ������
+    private ArrayList<Fragment> fragments; // Tabҳ���б�
+    private ImageView cursor;// ����ͼƬ
+    private TextView t1, t2, t3;// ҳ��ͷ��
+    private int offset = 0;// ����ͼƬƫ����
+    private int currIndex = 0;// ��ǰҳ�����
+    private int bmpW;// ����ͼƬ���
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        InitTextView();
+        InitImageView();
+        InitViewPager();
+
+    }
+
+    /**
+     * ��ʼ��ViewPager
+     */
+    private void InitViewPager() {
+        mPager = (ViewPager) findViewById(R.id.vPager);
+        fragments = new ArrayList<Fragment>();
+        Fragment fragment1 = new UserPage();
+        Fragment fragment2 = new MainPage();
+        Fragment fragment3 = new GamePage();
+        fragments.add(fragment1);
+        fragments.add(fragment2);
+        fragments.add(fragment3);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), fragments));
+        mPager.setCurrentItem(0);
+        mPager.setOnPageChangeListener(new MyOnPageChangeListener());
+        //mPager.addOnPageChangeListener(new MyOnPageChangeListener());
+    }
+
+    /**
+     * ��ʼ��ͷ��
+     */
+    private void InitTextView() {
+        t1 = (TextView) findViewById(R.id.text1);
+        t2 = (TextView) findViewById(R.id.text2);
+        t3 = (TextView) findViewById(R.id.text3);
+
+        t1.setOnClickListener(new MyOnClickListener(0));
+        t2.setOnClickListener(new MyOnClickListener(1));
+        t3.setOnClickListener(new MyOnClickListener(2));
+    }
+
+    /**
+     * ͷ��������
+     */
+    public class MyOnClickListener implements View.OnClickListener {
+        private int index = 0;
+
+        public MyOnClickListener(int i) {
+            index = i;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mPager.setCurrentItem(index);
+        }
+    };
+
+    /**
+     * ViewPager������
+     */
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+        ArrayList<Fragment> list;
+        public MyPagerAdapter(FragmentManager fm,ArrayList<Fragment> list) {
+            super(fm);
+            this.list = list;
+
+        }
+
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Fragment getItem(int arg0) {
+            return list.get(arg0);
+        }
+
+
+    }
+
+    /**
+     * ��ʼ������
+     */
+    private void InitImageView() {
+        cursor = (ImageView) findViewById(R.id.cursor);
+        bmpW = BitmapFactory.decodeResource(getResources(), R.drawable.a)
+                .getWidth();// ��ȡͼƬ���
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int screenW = dm.widthPixels;// ��ȡ�ֱ��ʿ��
+        offset = (screenW / 3 - bmpW) / 2;// ����ƫ����
+        Matrix matrix = new Matrix();
+        matrix.postTranslate(offset, 0);
+        cursor.setImageMatrix(matrix);// ���ö�����ʼλ��
+    }
+
+    /**
+     * ҳ���л�����
+     */
+    public class MyOnPageChangeListener implements OnPageChangeListener {
+
+        int one = offset * 2 + bmpW;// ҳ��1 -> ҳ��2 ƫ����
+        int two = one * 2;// ҳ��1 -> ҳ��3 ƫ����
+
+        @Override
+        public void onPageSelected(int arg0) {
+            Animation animation = null;
+            switch (arg0) {
+                case 0:
+                    if (currIndex == 1) {
+                        animation = new TranslateAnimation(one, 0, 0, 0);
+                    } else if (currIndex == 2) {
+                        animation = new TranslateAnimation(two, 0, 0, 0);
+                    }
+                    break;
+                case 1:
+                    if (currIndex == 0) {
+                        animation = new TranslateAnimation(offset, one, 0, 0);
+                    } else if (currIndex == 2) {
+                        animation = new TranslateAnimation(two, one, 0, 0);
+                    }
+                    break;
+                case 2:
+                    if (currIndex == 0) {
+                        animation = new TranslateAnimation(offset, two, 0, 0);
+                    } else if (currIndex == 1) {
+                        animation = new TranslateAnimation(one, two, 0, 0);
+                    }
+                    break;
+            }
+            currIndex = arg0;
+            animation.setFillAfter(true);// True:ͼƬͣ�ڶ�������λ��
+            animation.setDuration(300);
+            cursor.startAnimation(animation);
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+    }
+}
